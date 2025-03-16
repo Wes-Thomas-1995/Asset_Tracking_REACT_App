21c7d9c986f33a9a96eb5f987ed842f28a51b6f9
Wes Thomas <87914143+Wes-Thomas-1995@users.noreply.github.com>
2025-03-16 14:21:54 +0100

    UPDATE_5

--- DRF__API/API_CONNECTIONS/TESTING_ZONE.py
+++ DRF__API/API_CONNECTIONS/TESTING_ZONE.py
@@ -138,16 +138,6 @@ class SQL_DATA_SAVE():
         HOST                        = os.getenv("DB_HOST")
         ERROR_LIST                  = []
 
-        POSTGRES_HOSTNAME           = "ENKIINVESTMENTS-186.postgres.eu.pythonanywhere-services.com"
-        POSTGRES_HOST_PORT          = 10186
-        SSH_USER_NAME               = 'ENKIINVESTMENTS'
-        SSH_PASSWORD                = 'BRYONYBUTTON0104'
-        USERNAME                    = 'super'
-        PASSWORD                    = 'BRYONYBUTTON0104'
-        DB_NAME                     = 'myappdb'
-        HOST                        = '127.0.0.1'
-        ERROR_LIST                  = []
-
         with sshtunnel.SSHTunnelForwarder(
                 ('ssh.eu.pythonanywhere.com'),
                 ssh_username        = SSH_USER_NAME,
